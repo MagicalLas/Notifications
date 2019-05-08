a25e0c1fd16499f941de5616f6318f1779f7b479
Wonho ha <haho013039@gmail.com>
2019-05-08 18:27:59 +0900

    [IMPLEMENT] group add schedule method

--- test_group.py
+++ test_group.py
@@ -8,3 +8,8 @@ def test_create_group():
     assert sadistix_group.name == "sadistix_coders"
     assert sadistix_group.invite_key == "invite_key"
 
+def test_add_schedule_to_group():
+    sadistix_group = Group("Sadistix Coders", "Invite_key")
+    first_schedule = Schedule("Title", "Discription", datetime.datetime.now())
+    sadistix_group.add_schedule(first_schedule)
+    assert sadistix_group.schedule_count == 1
