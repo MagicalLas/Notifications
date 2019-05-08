7ec3f83b5642cf90cc9a2eb256b35b3215cc8940
Wonho ha <haho013039@gmail.com>
2019-05-08 18:27:36 +0900

    [MOVE] group test file

--- test_group.py
+++ test_group.py
@@ -1,5 +1,10 @@
+import datetime
+from group import Group
+from schedule import Schedule
+
 def test_create_group():
     sadistix_group = Group("Sadistix Coders", "Invite_key")
     assert isinstance(sadistix_group, Group)
     assert sadistix_group.name == "sadistix_coders"
     assert sadistix_group.invite_key == "invite_key"
+
