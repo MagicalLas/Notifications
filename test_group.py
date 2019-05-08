65f0065b6e6e443cb343c9d1dae9e11583bdc7cd
Wonho ha <haho013039@gmail.com>
2019-05-08 19:36:48 +0900

    [FORMAT] format to PEP8

--- test_group.py
+++ test_group.py
@@ -2,11 +2,13 @@ import datetime
 from group import Group
 from schedule import Schedule
 
+
 def test_create_group():
     sadistix_group = Group("Sadistix Coders", "Invite_key")
     assert isinstance(sadistix_group, Group)
     assert sadistix_group.name == "sadistix_coders"
     assert sadistix_group.invite_key == "Invite_key"
+
 
 def test_add_schedule_to_group():
     sadistix_group = Group("Sadistix Coders", "Invite_key")
