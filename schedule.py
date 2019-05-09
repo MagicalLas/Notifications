02a03c37db798b48d0de8db3a930ccb6092bac29
Wonho ha <haho013039@gmail.com>
2019-05-09 23:45:26 +0900

    [IMPLEMENT] Schedule Create Form

--- schedule.py
+++ schedule.py
@@ -8,10 +8,11 @@ class Schedule(object):
         self._title = title
         self._desciption = description
         self._date = date
+        self._html_template = """<div class="row"><div class="col-sm-6"><div class="card" style="width: 20rem;"><img class="card-img-top" src="https://github.com/Las-Wonho/Notifications/raw/master/icon.png" alt="Card image cap"><div class="card-body"><h4 class="card-title">{title}</h4><p class="card-text">{des}</p></div></div></div><div class="col-sm-6"> <div class="card" style="width: 20rem;"><img class="card-img-top" src="https://github.com/Las-Wonho/Notifications/raw/master/icon.png" alt="Card image cap"><div class="card-body"><h4 class="card-title">New Schedule</h4><p class="card-text">새로운 스케줄을 생성할 수 있습니다.</p><a class="btn btn-primary" onmousedown="CreateNewScheduleFrom()">새로운 스케줄 생성하기</a></div></div></div></div>"""
 
     @property
     def rendered_html(self):
-        return """<div class="card" style="width: 20rem;"><img class="card-img-top" src="https://github.com/Las-Wonho/Notifications/raw/master/icon.png" alt="Card image cap"><div class="card-body"><h4 class="card-title">{title}</h4><p class="card-text">{des}</p></div></div>""".format(title=self._title, des=self._desciption)
+        return self._html_template.format(title=self._title, des=self._desciption)
 
     def __str__(self):
         json_data = {
